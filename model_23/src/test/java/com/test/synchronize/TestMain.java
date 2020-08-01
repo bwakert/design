35b082df8a7f71b3768381641437265c384c450b
别伟岸 <ssi-biewa@dfmc.com.cn>
2020-08-01 10:59:59 +0800

    模式改变

--- model_23/src/test/java/com/test/synchronize/TestMain.java
+++ model_23/src/test/java/com/test/synchronize/TestMain.java
@@ -4,6 +4,7 @@ public class TestMain {
     public static void main(String[] args) {
         TestSynchronize testSynchronize = new TestSynchronize();
         new Thread(() -> testSynchronize.test1()).start();
+        TestSynchronize testSynchronize1 = new TestSynchronize();
         new Thread(() -> testSynchronize.test2()).start();
     }
 }
