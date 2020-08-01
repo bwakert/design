5e0bf7d073f435ccbdebec87f6584f89215620e8
别伟岸 <ssi-biewa@dfmc.com.cn>
2020-08-01 11:34:30 +0800

    模式测试

--- model_23/src/test/java/com/test/synchronize/TestMain.java
+++ model_23/src/test/java/com/test/synchronize/TestMain.java
@@ -3,8 +3,11 @@ package com.test.synchronize;
 public class TestMain {
     public static void main(String[] args) {
         TestSynchronize testSynchronize = new TestSynchronize();
+
         new Thread(() -> testSynchronize.test1()).start();
-        TestSynchronize testSynchronize1 = new TestSynchronize();
+
+        TestSynchronize testSynchronize12 = new TestSynchronize();
+
         new Thread(() -> testSynchronize.test2()).start();
     }
 }
